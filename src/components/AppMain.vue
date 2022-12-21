9cda71936eefb3419aa0396e7856b85764ae47a6
RominaMalimban <romina.lyn@hotmail.it>
2022-12-22 00:57:42 +0100

    realizzazione componente Newsletter

---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+import Newsletter from './Newsletter.vue';
+
+export default {
+    name: "AppMain",
+    components: {
+        Newsletter
+    }
+}
+</script>
+
+<template>
+
+    <!-- <section>
+        <div>
+
+        </div>
+    </section> -->
+
+    <Newsletter />
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as*;
+@use '../styles/partials/mixins' as*;
+
+// section {
+//     background-image: url(../../public/img/promise-background.jpg);
+//     background-position: left center;
+
+//     // debug
+//     height: 400px;
+// }
+</style>
---
+++ src/components/Newsletter.vue
@@ -0,0 +1,64 @@
+<script>
+
+
+export default {
+    name: "Newsletter",
+}
+</script>
+
+<template>
+
+    <section>
+        <div class="container">
+            <h3>Sign up to our Newsletter</h3>
+            <form>
+                <input type="text">
+                <button type="submit">SUBSCRIBE</button>
+                <span>(We do not share your data with anybody, and only use it for its intended purpose)</span>
+            </form>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as*;
+@use '../styles/partials/mixins' as*;
+
+section {
+    background-image: url(/img/newsletter_background.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center top;
+    padding: 70px 0;
+    text-align: center;
+
+    h3 {
+        color: $gray-title;
+        margin-bottom: 50px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        input,
+        button {
+            width: 40%;
+            padding: 10px;
+        }
+
+        button {
+            color: #fff;
+            background-color: $primary;
+            border: 1px solid $primary;
+            border-radius: 30px;
+            margin: 25px 0;
+        }
+
+        span {
+            font-size: $small-font;
+        }
+    }
+}
+</style>
